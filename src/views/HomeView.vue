0849eb26a8efa9ab9ec568e781215e4d85067124
SpookedSymphony <139538882+SpookedSymphony@users.noreply.github.com>
2023-09-24 21:05:18 +1000

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,7 @@
+<script setup>
+  import Amelia from '../components/Amelia.vue'
+</script>
+
 <style scoped>
 .flex-container {
   display: flex;
@@ -15,11 +19,6 @@ h1 {
 </style>
 
 <template>
-  <main>
-    <h1>Welcome to Amelia's Website!</h1>
-    <p>Feel free to look around (: </p>
-  </main>
-  <img src="favicon.png" alt="Ghost" style="width:200px;height:200px;">
-  <a href="https://www.linkedin.com/in/amelia-schultz-9b2a6a192/" target="_blank">Visit my LinkedIn!</a>
+  <Amelia/>
 </template>
 
