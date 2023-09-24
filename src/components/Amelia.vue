14e915c74fd427e9a92e9129fc09843d293ac8c4
SpookedSymphony <139538882+SpookedSymphony@users.noreply.github.com>
2023-09-24 21:04:43 +1000

    Add files via upload

---
+++ src/components/Amelia.vue
@@ -0,0 +1,8 @@
+<template>
+    <main>
+        <h1>Welcome to Amelia's Website!</h1>
+        <p>Feel free to look around (: </p>
+    </main>
+    <img src="favicon.png" alt="Ghost" style="width:200px;height:200px;">
+    <a href="https://www.linkedin.com/in/amelia-schultz-9b2a6a192/" target="_blank">Visit my LinkedIn!</a>
+</template>
